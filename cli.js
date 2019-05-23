a4b81e2e8a0ba93e671f1549524672f72e9ae1c1
Michael Ekpang <weezyval93@gmail.com>
2019-05-23 01:36:52 +0100

    add cli.js file

---
+++ cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+blue=`tput setaf 4`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+check_dep() {
+  echo "${blue}Checking unused dependcies please wait...${reset}";
+
+  for dep in $(jq -r '.dependencies | keys | .[]' package.json); do
+    if ! grep "from\ .*$dep.*" -Rq --exclude-dir="node_modules" .; then
+      echo "You can probably remove "${green}"$dep"${reset}"";
+    fi
+  done
+}
+
+main(){
+  check_dep
+}
+main
