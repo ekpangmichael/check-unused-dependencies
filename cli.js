b9d59135894b9bbdbfde2f7abcf2bbac1e52aa39
Michael Ekpang <weezyval93@gmail.com>
2019-05-27 18:37:37 +0100

    removed required check

--- cli.js
+++ cli.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+
 blue=`tput setaf 4`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
 check_dep() {
-  echo "${blue}Checking unused dependcies please wait...${reset}";
+  echo "${blue}Scanning project for unused dependencies, please wait...${reset}";
 
   for dep in $(jq -r '.dependencies | keys | .[]' package.json); do
-    if ! grep "from\ .*$dep.*\|require\(.*$dep.*\)" -Rq --exclude-dir="node_modules" .; then
+    if ! grep "from\ .*$dep.*"  -Rq --exclude-dir="node_modules" .; 
+    then
       echo "You can probably remove "${green}"$dep"${reset}"";
     fi
   done
